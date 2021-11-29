4ebf80bfe661d10286a7d6d4005bd685f8843c1c
Mark Morcos <markmorcos1999@gmail.com>
2021-11-28 22:08:28 -0800

    working bluetooth module

---
+++ Hardware/logic-board/main.py
@@ -0,0 +1,50 @@
+import os
+import subprocess
+import time
+import re
+
+def init_bluetooth():
+    # Turn on bluetooth
+    os.system('rfkill unblock bluetooth')
+    # Make rpi discoverable
+    os.system('sudo hciconfig hci0 piscan')
+    # Accept any incoming connection
+    connect = subprocess.Popen(['bt-agent', '--capability=NoInputNoOutput'], shell=False, stdout=subprocess.PIPE)
+    
+    prev_output = ""
+    while True:
+        try:
+            output = connect.stdout.readline()
+        except:
+            continue
+        if connect.poll() is not None:
+            break
+        if output:
+            # Convert from byte type to string
+            output = output[:len(output)-1].decode()
+            print(output)
+            if "Device:" in output:
+                # output = Name + UID of device
+                output = output[:output.rindex('for')-1]       
+                if prev_output == output:
+                    # Device is connected
+                    time.sleep(6)
+                    connect.terminate()
+                    return 0
+                else:
+                    prev_output = output
+    return 1
+
+def main():
+    failed = init_bluetooth()
+    if failed == 1:
+        print('Connecting Failed')
+    else:
+        print('Device Connected Successfuly')
+
+if __name__ == "__main__":
+    main()
+
+'''
+outt = subprocess.check_output('bt-agent --capability=NoInputNoOutput', shell=True)
+'''
