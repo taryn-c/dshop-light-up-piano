59673bb3e53626e1d180b22b514f645e9c15d2c8
Mark Morcos <markmorcos1999@gmail.com>
2021-12-28 10:04:46 -0800

    remove gpio logic

--- Hardware/logic-board/rpi-logic.py
+++ Hardware/logic-board/rpi-logic.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python3
 import serial
-import RPi.GPIO as GPIO
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(19, GPIO.OUT)
 
 if __name__ == '__main__':
-    GPIO.output(19, True)
     ser = serial.Serial('/dev/ttyACM0', 57600, timeout=0)
     ser.reset_input_buffer()
     while True:
