ed460aa701efe7c2888cf3f81328cd757a1de266
Mark Morcos <markmorcos1999@gmail.com>
2022-03-30 00:03:53 -0700

    try creating serial connection

--- Hardware/logic-board/main.py
+++ Hardware/logic-board/main.py
@@ -31,17 +31,44 @@ def init_bluetooth():
                     # Turn discoverable off and terminate scan 
                     os.system('bluetoothctl discoverable off')
                     connect.terminate()
+                    print('Device Connected Successfuly')
                     return 0
                 else:
                     prev_output = output
+    print('Connecting Failed')
+    return 1
+
+def init_serial_comm():
+    serial_result = subprocess.Popen(['sudo', 'rfcomm', 'watch', 'hci0'], shell=False, stdout=subprocess.PIPE)
+    while True:
+        try:
+            output = serial_result.stdout.readline()
+        except:
+            continue
+        if serial_result.poll() is not None:
+            break
+        if output:
+            # Convert from byte type to string
+            output = output[:len(output)-1].decode()
+            print(output)
+            if 'Connection from' in output:
+                print('Serial Connection Created Successfuly')
+                return 0
+            elif 'bind RFCOMM socket: Address already in use' in output:
+                subprocess.Popen(['top', '-b', '|', 'grep', '\' rfcomm\''], shell=False, stdout=subprocess.PIPE)
+                try:
+                    output = serial_result.stdout.readline()
+                except:
+                    continue
+                if output:
+                    output = output[:len(output)-1].decode()
+                    os.system('kill ' + output[:output.index(' ')-1])
     return 1
 
 def main():
     failed = init_bluetooth()
-    if failed == 1:
-        print('Connecting Failed')
-    else:
-        print('Device Connected Successfuly')
+    if not failed:
+        init_serial_comm()
 
 if __name__ == "__main__":
     main()
