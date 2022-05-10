17f5331489a21b064d53525bb158cafd5d66eea5
Mark Morcos <markmorcos1999@gmail.com>
2022-05-09 22:19:09 -0700

    fixed notes + uploaded new library

--- Hardware/logic-board/rpi-logic.py
+++ Hardware/logic-board/rpi-logic.py
@@ -4,18 +4,18 @@ import pygame
 from time import sleep
 
 dict = {
-    '100000000000': 'piano/c4.ogg',
-    '010000000000': 'piano/d4.ogg',
-    '001000000000': 'piano/e4.ogg',
-    '000100000000': 'piano/f4.ogg',
-    '000010000000': 'piano/g4.ogg',
-    '000001000000': 'piano/a5.ogg',
-    '000000100000': 'piano/b5.ogg',
-    '000000010000': 'piano/c5.ogg',
-    '000000001000': 'piano/d5.ogg',
-    '000000000100': 'piano/e5.ogg',
-    '000000000010': 'piano/f5.ogg',
-    '000000000001': 'piano/g5.ogg',
+    '100000000000': 'piano2/C5.mp3',
+    '010000000000': 'piano2/Db5.mp3',
+    '001000000000': 'piano2/D5.mp3',
+    '000100000000': 'piano2/Eb5.mp3',
+    '000010000000': 'piano2/E5.mp3',
+    '000001000000': 'piano2/F5.mp3',
+    '000000100000': 'piano2/Gb5.mp3',
+    '000000010000': 'piano2/G5.mp3',
+    '000000001000': 'piano2/Ab5.mp3',
+    '000000000100': 'piano2/A5.mp3',
+    '000000000010': 'piano2/Bb5.mp3',
+    '000000000001': 'piano2/B5.mp3',
 }
 
 zero_char = '0'
@@ -24,16 +24,15 @@ one_char = '1'
 def play_note(file_name):
     print('init =', pygame.mixer.get_init())
     print('channels =', pygame.mixer.get_num_channels())
-    snd = pygame.mixer.Sound(file_name)
-    snd.play()
-    print('length =', snd.get_length())
+    pygame.mixer.music.load(file_name)
+    pygame.mixer.music.play()
+    print(file_name)
 
 def convert_binary_to_piano_note(line):
     while line.count('1') > 0:
         file = dict[line]
         play_note(file)
         line = line.replace(one_char, zero_char, 1)
-    
 
 if __name__ == '__main__':
     pygame.mixer.init()
